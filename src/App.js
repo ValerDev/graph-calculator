02bfb3b71b5b9f1558289a29b1cc49aeffd4dd88
gorhovagimyan <gor.hovagimyan@gmail.com>
2020-12-18 18:31:54 +0400

    added functionality

--- src/App.js
+++ src/App.js
@@ -3,27 +3,35 @@ import './App.css';
 
 
 class App extends React.Component {
+  state = {
+    func: '',
+    max: '',
+    min: '',
+    colors: ['red', 'green', 'blue', 'yellow', 'purple', 'orange'],
+    randomNumber: 1,
+    x: [],
+    y: [],
+    c: null,
+    ctx: null,
+    centerX: null,
+    centerY: null,
+    blocker: false,
+  }
   componentDidMount() {
 
     //create canvas
     const c = document.getElementById("myCanvas")
     const ctx = c.getContext("2d")
 
-    //colors
-    const colors = ['red', 'green', 'blue', 'yellow', 'purple', 'orange']
-    let randomNumber = Math.floor(Math.random() * (colors.length - 1))
-
     //center
     const center = [c.width / 2, c.height / 2]
-
-    //min, max
-
-    const Xmin = 0;
-    const Xmax = c.width;
-    const Ymin = 0;
-    const Ymax = c.height;
-
-    // X and Y 
+    this.setState({
+      c: c,
+      ctx: ctx,
+      centerX: center[0],
+      centerY: center[1],
+    })
+    // X and Y lines
     ctx.beginPath();
     ctx.strokeStyle = 'white';
     ctx.moveTo(0, 250)
@@ -37,34 +45,99 @@ class App extends React.Component {
     ctx.fillText("^", 241, 25);
     ctx.fillText(">", 490, 260);
     ctx.stroke();
-
-
-    //graph
-    ctx.beginPath();
-    ctx.strokeStyle = colors[randomNumber];
-
-    ctx.moveTo(center[0], center[1])
-    ctx.lineTo(Xmax, Ymin)
-
-    ctx.moveTo(center[0], center[1]);
-    ctx.quadraticCurveTo(Xmin, Ymax / 2, Xmin, Ymax);
-
-    ctx.moveTo(center[0], center[1]);
-    ctx.quadraticCurveTo(Xmax, Ymax / 2, Xmax, Ymin);
-    ctx.lineWidth = 3;
-    ctx.stroke();
-
+    ctx.lineWidth = 2;
   }
+
+  // create graph function
+
+  plotFunction = (e) => {
+    this.setState({
+      randomNumber: Math.floor(Math.random() * (this.state.colors.length - 1))
+    })
+    e.preventDefault()
+    let exp = this.state.func
+    this.state.ctx.beginPath();
+    this.state.ctx.strokeStyle = this.state.colors[this.state.randomNumber];
+    for (let i = this.state.min; i <= this.state.max; i++) {
+      let expToArr = exp.split('')
+
+      expToArr.forEach((e, index) => {
+        if (e === 'x') {
+          expToArr[index] = i;
+        } else if (e === "^") {
+          expToArr[index] = ',';
+          expToArr.unshift("Math.pow(");
+          expToArr.push(')');
+          eval(expToArr.join(''));
+        } else if (expToArr.join('').split("(")[0] === "sqrt") {
+          expToArr.unshift("Math.")
+        }
+
+      })
+      this.state.x.push(i * 10);
+      this.state.y.push(+eval(expToArr.join('')) * 5)
+
+      this.setState({
+        x: [],
+        y: [],
+      })
+    }
+    this.createGraph()
+  }
+  createGraph = () => {
+    if (!this.state.blocker) {
+      this.state.ctx.moveTo(
+        this.state.centerX + this.state.x[0],
+        this.state.centerY - this.state.y[0]);
+      this.state.ctx.lineTo(
+        this.state.centerX + this.state.x[1],
+        this.state.centerY - this.state.y[1]);
+      this.setState({
+        blocker: true
+      })
+    }
+    for (let i = 1; i < this.state.x.length; i++) {
+      this.state.ctx.lineTo(
+        this.state.centerX + this.state.x[i],
+        this.state.centerY - this.state.y[i]);
+    }
+    this.state.ctx.stroke()
+
+    this.setState({
+      blocker: false
+    })
+  }
+
+  //inputs handeling
+
+  handlePlotFunction = (e) => {
+    this.setState({
+      func: e.target.value
+    })
+  }
+
+  handleMaxValue = (e) => {
+    this.setState({
+      max: Number(e.target.value)
+    })
+  }
+
+  handleMinValue = (e) => {
+    this.setState({
+      min: Number(e.target.value)
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <form className="inputs-block">
-          <input onChange={e => e.target.value} placeholder="Mathematical expression.." />
+          <input onChange={this.handlePlotFunction} placeholder="Mathematical expression.." />
           <div className="digital-range">
-            <input onChange={e => e.target.value} placeholder="Minimal value.." />
-            <input onChange={e => e.target.value} placeholder="Maximal value.." />
+            <input onChange={this.handleMinValue} placeholder="Minimal value.." />
+            <input onChange={this.handleMaxValue} placeholder="Maximal value.." />
           </div>
-          <button type="submit">Plot function</button>
+          <button onClick={this.plotFunction}>Plot function</button>
         </form>
         <canvas id="myCanvas" width="500" height="500" ></canvas>
       </div>
