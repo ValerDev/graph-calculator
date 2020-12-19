02a50e3d3a83e4a5e46a375517c6fccb3af5f5cd
Valer <valerghahramanyan@gmail.com>
2020-12-20 00:16:01 +0400

    bug fix

--- src/App.js
+++ src/App.js
@@ -45,10 +45,10 @@ class App extends React.Component {
     ctx.fillText("^", 241, 40);
     ctx.fillText(">", 475, 260);
     ctx.font = "8px Cursive";
-    for (let i = -20, j = 40; i < 0, j < 250; i += 4, j += 40) {
+    for (let i = -20, j = 43; i < 0, j < 250; i += 4, j += 40) {
       ctx.fillText(i, j, 265);
     }
-    for (let i = 4, j = 280; i < 20, j < 460; i += 4, j += 40) {
+    for (let i = 4, j = 285; i < 20, j < 460; i += 4, j += 40) {
       ctx.fillText(i, j, 265);
     }
 
@@ -67,7 +67,7 @@ class App extends React.Component {
 
   plotFunction = (e) => {
     this.setState({
-      randomNumber: Math.floor(Math.random() * (this.state.colors.length ))
+      randomNumber: Math.floor(Math.random() * (this.state.colors.length))
     })
     e.preventDefault()
     let exp = this.state.func
@@ -76,20 +76,39 @@ class App extends React.Component {
     for (let i = this.state.min; i <= this.state.max; i++) {
       let expToArr = exp.split('')
 
-      expToArr.forEach((e, index) => {
-        if (e === 'x') {
-          expToArr[index] = i;
-        } else if (e === "^") {
-          expToArr[index] = ',';
-          expToArr.unshift("Math.pow(");
-          expToArr.push(')');
-          eval(expToArr.join(''));
-        } else if (expToArr.join('').split("(")[0] === "sqrt") {
-          expToArr.unshift("Math.")
-        }
-      })
-      this.state.x.push(i * 10);
-      this.state.y.push(+eval(expToArr.join('')) * 10)
+      if (expToArr.join('').includes('x')) {
+        expToArr.forEach((e, index) => {
+          if (e === "x") {
+            expToArr[index] = `(${i})`
+          }
+        })
+      }
+      if (expToArr.join('').includes('^')) {
+        expToArr.forEach((e, index) => {
+          if (e === "^") {
+            expToArr[expToArr.indexOf('^')] = '**';
+          }
+        })
+      }
+      if (expToArr.join('').includes("sqrt")) {
+        console.log("asd", expToArr.join(''));
+        expToArr = expToArr.join('').replace(/sqrt/gi, "Math.sqrt").split('')
+      }
+      console.log("verjnakan  ", expToArr)
+      try {
+        this.state.x.push(i * 10);
+        this.state.y.push(+eval(expToArr.join('')) * 10)
+        this.state.sqrt = true
+      }
+      catch (err) {
+        document.querySelector(".App").innerHTML = `
+        <div class="error-block">
+          <p class="error">
+            You have entered an incorrect math expression, please read the notes to use the app correctly 
+          </p>
+          <p class="back" onClick="location.reload()">Back to app</p>
+        </div>`
+      }
 
       this.setState({
         x: [],
@@ -141,20 +160,52 @@ class App extends React.Component {
     })
   }
 
+  // back to app 
+
+  backToApp = () => {
+    this.componentDidMount();
+  }
   render() {
     return (
       <div className="App">
-        <form className="inputs-block">
-          <label htmlFor="expressonY"  >
-            &nbsp; Y =
+        <div className="main-content">
+
+          <form className="inputs-block">
+            <label htmlFor="expressonY"  >
+              &nbsp; Y =
             <input id="expressonY" onChange={this.handlePlotFunction} placeholder="Mathematical expression.." autoComplete="off" />
-          </label>
-          <div className="digital-range">
-            <input onChange={this.handleMinValue} placeholder="Min value of x.." />
-            <input onChange={this.handleMaxValue} placeholder="Max value of x.." />
+            </label>
+            <div className="digital-range">
+              <input onChange={this.handleMinValue} placeholder="Min value of x.." />
+              <input onChange={this.handleMaxValue} placeholder="Max value of x.." />
+            </div>
+            <button onClick={this.plotFunction}>Plot function</button>
+          </form>
+          <div className="docs">
+            <h2 className='title'>Notes</h2>
+            <span>
+              <ul>
+                <li>
+                  Please enter the variable as ‘x’( e.g. 1 / x).
+            </li>
+                <li>
+                  Enter the expression without equation. (Do not do y=x+4. enter only x+4 ).
+            </li>
+                <li>
+                  For power expressions you can use both the “^” sign and the “*” sign (e.g.  x^2 shows the same result as x*x).
+            </li>
+                <li>
+                  For plotting the square root use sqrt() function. (e.g.  sqrt(x) ).
+            </li>
+                <li>
+                  You can also choose the interval from minimum to maximum to plot the function.
+            </li>
+              </ul>
+              <br />
+              Have fun!
+            </span>
           </div>
-          <button onClick={this.plotFunction}>Plot function</button>
-        </form>
+        </div>
         <canvas id="myCanvas" width="500" height="500" ></canvas>
       </div>
     );
